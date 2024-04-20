50661bc3f1dcf892b4c73bf15c63e73155edbad8
Chuks Enedeh <c.enedeh@televic.com>
2024-04-20 21:27:48 +0100

    expose product endpoint

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Service to the container
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
 // Configure HTTP request pipeline.
-
+app.MapCarter();
 app.Run();
